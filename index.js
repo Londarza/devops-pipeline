3200095a680b101094940bb35ea3c2b3a8944ef0
Londarza <lautaro.ondarza@gmail.com>
2025-12-17 11:16:29 -0300

    se prueba config inicial para proyecto pipelines, con node

---
+++ index.js
@@ -0,0 +1,2 @@
+console.log ('hello devops');
+console.log('Im learning CICD pipeline with github actions');
---
+++ test.js
@@ -0,0 +1,5 @@
+console.log('starting tests...');
+
+setTimeout(() => console.log('waiting for tests...'), 3000);
+
+console.log('tests completed successfully!');
